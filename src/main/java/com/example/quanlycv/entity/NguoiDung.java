77b1c9a38364ea146919f7b377113c04dcf1fef3
giangvanhieuPH56055 <149453334+giangvanhieuPH56055@users.noreply.github.com>
2024-09-28 15:14:42 +0700

    commit entity nguoi dung

--- src/main/java/com/example/quanlycv/entity/NguoiDung.java
+++ src/main/java/com/example/quanlycv/entity/NguoiDung.java
@@ -1,41 +1,24 @@
-package com.example.quanlycv.entity;
+package com.example.quanlycv.Entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity
-@Table(name = "Nguoi_Dung")
-@AllArgsConstructor
 @NoArgsConstructor
-@Data
+@AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "Nguoi_Dung")
 public class NguoiDung {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "nguoi_dung_id")
-     Integer id;
+    private int nguoi_dung_id;
+    @Column(name = "ho_ten")
+    private String hoTen;
 
-    @Column(name = "ho_ten",nullable = false,  length = 100)
-     String hoTen;
 
-    @Column(name = "email",nullable = false, unique = true, length = 100)
-     String email;
 
-    @Column(name = "sdt", length = 15)
-     String sdt;
-
-    @Column(name = "password")
-     String password;
-
-    @ManyToOne
-    @JoinColumn(name = "vai_tro_id")
-     VaiTro vaiTro;
-
-    @ManyToOne
-    @JoinColumn(name = "nhan_vien_id")
-     NhanVien nhanVien;
-
-    @Column(name = "trang_thai", columnDefinition = "BIT DEFAULT 1")
-     Boolean trangThai;
 }

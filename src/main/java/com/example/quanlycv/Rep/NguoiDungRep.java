ab012750928372514596939816a7a4196b6b795d
giangvanhieuPH56055 <149453334+giangvanhieuPH56055@users.noreply.github.com>
2024-10-03 18:19:32 +0700

    update, validation, change-status

--- src/main/java/com/example/quanlycv/Rep/NguoiDungRep.java
+++ src/main/java/com/example/quanlycv/Rep/NguoiDungRep.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NguoiDungRep extends JpaRepository<NguoiDung, Integer> {
+
 }
--- src/main/java/com/example/quanlycv/restcontroller/RestRoleController.java
+++ src/main/java/com/example/quanlycv/restcontroller/RestRoleController.java
@@ -1,8 +1,10 @@
 package com.example.quanlycv.restcontroller;
 
 import com.example.quanlycv.Service.RolesService;
+import com.example.quanlycv.entity.NguoiDung;
 import com.example.quanlycv.entity.NhanVien;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,19 +14,16 @@ public class RestRoleController {
     @Autowired
     RolesService rolesService;
 
-//    @GetMapping("/admin/role/{id}")
-//    public String getNhanVienID(@PathVariable("id") Integer id){
-//        System.out.println("TestGetID: "+id);
-//        NhanVien nv = rolesService.getNhanVienById(id);
-//        System.out.println("TestGet dữ liệu id: "+nv);
-//        return "name:"+id;
-//    }
 
     @GetMapping("/admin/role/{id}")
     public NhanVien getNhanVienID(@PathVariable("id") Integer id){
-        //System.out.println("TestGetID: "+id);
         NhanVien nv = rolesService.getNhanVienById(id);
-        //System.out.println("TestGet dữ liệu id: "+nv);
+        return nv;
+    }
+
+    @GetMapping("/admin/role/edit/{id}")
+    public NguoiDung edit(@PathVariable("id") Integer id, Model model){
+        NguoiDung nv = rolesService.getNguoiDungById(id);
         return nv;
     }
 }

3660a7aa16ed1b373f739b6b824781ba263a9982
long07122004 <longhvph35733@fpt.edu.vn>
2024-10-04 09:27:26 +0700

    commit

--- src/main/java/com/example/quanlycv/Rep/TuyenDungRep.java
+++ src/main/java/com/example/quanlycv/Rep/TuyenDungRep.java
@@ -4,15 +4,24 @@ import com.example.quanlycv.entity.QlTuyenDung;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TuyenDungRep extends JpaRepository<QlTuyenDung,Integer> {
 
     Page<QlTuyenDung> findAll(Pageable pageable);
+//    @Query("SELECT t FROM QlTuyenDung t WHERE " +
+//            "LOWER(t.maDot) LIKE %:keyword% OR " +
+//            "LOWER(t.tenDot) LIKE %:keyword% ")
+//           // "LOWER(t.noiDung) LIKE %:keyword%")
+//
+//    Page<QlTuyenDung> searchByKeyword(@Param("keyword") String keyword, Pageable pageable);
 
-//    @Query("SELECT td FROM QlTuyenDung td WHERE td.maDot LIKE %?1% OR td.tenDot LIKE %?1% OR td.noiDung LIKE %?1%  OR td.deadline LIKE %?1% ")
-//    List<QlTuyenDung> searchByKeyword(String keyword);
+
+    @Query("SELECT td FROM QlTuyenDung td WHERE td.maDot LIKE %?1% OR td.tenDot LIKE %?1% OR td.noiDung LIKE %?1%   ")
+    Page<QlTuyenDung> searchByKeyword(@Param("keyword") String keyword, Pageable pageable);
 
 //    List<QlTuyenDung> findByMaDotContainingOrTenDotContainingOrNoiDungContaining(String maDot, String tenDot, String noiDung, Date deadline);
 }
--- src/main/java/com/example/quanlycv/entity/NguoiDung.java
+++ src/main/java/com/example/quanlycv/entity/NguoiDung.java
@@ -16,9 +16,34 @@ import lombok.Setter;
 public class NguoiDung {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int nguoi_dung_id;
-    @Column(name = "ho_ten")
-    private String hoTen;
+    @Column(name = "nguoi_dung_id")
+    Integer id;
+
+    @Column(name = "ho_ten",nullable = false,  length = 100)
+    String hoTen;
+
+    @Column(name = "email",nullable = false, unique = true, length = 100)
+    String email;
+
+    @Column(name = "sdt", length = 15)
+    String sdt;
+
+    @Column(name = "password")
+    String password;
+
+    @ManyToOne
+    @JoinColumn(name = "vai_tro_id")
+    VaiTro vaiTro;
+
+    @ManyToOne
+    @JoinColumn(name = "nhan_vien_id")
+    NhanVien nhanVien;
+
+    @Column(name = "trang_thai", columnDefinition = "BIT DEFAULT 1")
+    Boolean trangThai;
+
+
+
 
 
 
